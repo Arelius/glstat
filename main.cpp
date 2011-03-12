4565802fd0f50ce8b88763283bdf8df41a561b08
Nicholas 'Indy' Ray <arelius@gmail.com>
2011-03-11 16:08:54 -0800

    Macros, go!

--- main.cpp
+++ main.cpp
@@ -5,20 +5,13 @@
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(1280, 720);
-	glutInitWindowPosition(100, 100);
-	glutCreateWindow("glstat\n");
+    glutCreateWindow("glstat\n");
 
-    GLint maxbuffers;
-    glGetIntegerv(GL_MAX_DRAW_BUFFERS, &maxbuffers);
-    printf("Max supported draw buffers: %d\n", maxbuffers);
+#define GL_INTEGER(x) { GLint i; glGetIntegerv(x, &i); printf(#x ": %d\n", i); }
 
-    GLint maxattribs;
-    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &maxattribs);
-    printf("Max supported vertex attributes: %d\n", maxattribs);
+    GL_INTEGER(GL_MAX_DRAW_BUFFERS);
+    GL_INTEGER(GL_MAX_VERTEX_ATTRIBS);
+    GL_INTEGER(GL_MAX_ELEMENTS_VERTICES);
 
-    GLint maxvertex;
-    glGetIntegerv(GL_MAX_ELEMENTS_VERTICES, &maxvertex);
-    printf("Max supported element vertices: %d\n", maxvertex);
+    return 0;
 }
